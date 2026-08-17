47c527177d67bf79e2d0d63b04c632d1347764f4
Dishit-07 <dishitmishra@gmail.com>
2026-08-17 21:52:15 +0530

    Time: 0 ms (100%), Space: 55.3 MB (16.67%) - LeetHub

---
+++ 4024-nearest-available-drone/4024-nearest-available-drone.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int nearestDrone(vector<vector<int>>& drones, vector<int>& target) {
+        int ans = -1;
+        int n = drones.size();
+        int minDist = INT_MAX;
+        
+        for(int i = 0; i<n;i++){
+            int dist = abs(drones[i][0] - target[0]) + abs(drones[i][1]- target[1]);
+            int range = drones[i][2];
+            if(dist<= range){
+                if(dist < minDist){
+                minDist = dist;
+                ans = i;
+                }
+            }
+            
+        }
+        return ans;
+    }
+};
